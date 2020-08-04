8ba99d55e74d1b2be45a83bb5c19096c661b04d3
Roman Revunenkov <romik.rev@gmail.com>
2020-08-04 12:44:10 +0300

    add 1st Task

---
+++ assets/js/index.js
@@ -0,0 +1,51 @@
+'use strict';
+
+// Variable Tasks:
+
+console.log("\nVariable Tasks:\n\n");
+// 1) multiplication
+let a = 3;
+let b = 7;
+
+console.log("1) multiplication result: " + (a * b));
+
+// 2) Division
+let c = 27;
+let d = 9;
+
+console.log("2) Division result: " + (c / d));
+
+// 3) Addition
+let e = 13;
+let f = 11;
+
+console.log("3) Addition result: " + (e + f));
+
+// 4) Defining Variables:
+let number = 11;
+let boolean = true;
+let char = "java script";
+let char2 = "100";
+
+console.log(`4) Defining Variable result: \n   number: ${number}\n   boolean: ${boolean}  \n   char: ${char}  \n   char2: ${char2}`);
+
+// 5) Shirt operation:
+let num = 1;
+
+num +=  11;
+num -= 11;
+num *= 11;
+num /= 11;
+num ++;
+num --;
+
+console.log(`5) Shirt operation result: ${num}`);
+
+
+// Prompt tasks:
+console.log("\nPrompt Tasks:\n\n");
+
+//  1)  
+//  2)
+//  3)
+//  4)    
